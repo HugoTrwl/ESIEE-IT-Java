1586fc08afe2da1981ad9c35e9adf94e4146e96b
HugoTrwl <82870172+HugoTrwl@users.noreply.github.com>
2021-04-26 12:14:54 +0200

    Add files via upload

---
+++ src/StarWars_BDD/ModifTitle.java
@@ -0,0 +1,32 @@
+package StarWars_BDD;
+
+import java.sql.*;
+
+public class ModifTitle {
+	public static void ModifTitle(int id) {
+		try {			
+			String strClassName = "org.sqlite.JDBC";
+			String dbName= "E:/Content/Travail/Itescia/Cours/L3/Phase 3/Java/Cours4/BDD/BDD_StarWarsTP.db"; 
+			String login= ""; 
+			String motdepasse= ""; 
+			String strUrl = "jdbc:sqlite:" +  dbName;
+			
+			String strModif = "UPDATE Acces "
+					+ "SET title = 'new title'"
+					+ "WHERE id = " + id + ";";
+
+			Class.forName(strClassName);
+			Connection conn = DriverManager.getConnection(strUrl, login, motdepasse);
+			
+			Statement stAddUser = conn.createStatement();
+			stAddUser.executeUpdate(strModif);
+			
+			conn.close();
+		}
+		catch(ClassNotFoundException e) {  
+			System.err.println("Driver non chargé !");  e.printStackTrace();
+		} catch(SQLException e) {
+			System.err.println("Autre erreur !");  e.printStackTrace();
+		}
+	}
+}
